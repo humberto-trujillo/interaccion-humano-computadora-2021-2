4c687ce3442fd9c7758afe1fe03e1dd719e5cf05
Humberto Trujillo <htp.tito@gmail.com>
2021-09-17 19:09:27 -0700

    Inventory part 1

---
+++ practicas/Assets/Scripts/InventorySlot.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySlot : MonoBehaviour
+{
+    public void TestSlot()
+    {
+        Debug.Log("Click a "+ gameObject.name);
+    }
+}
